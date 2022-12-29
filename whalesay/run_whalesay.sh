5e9361b48b03419bf34703e5530fdb0e517647ca
Danny Zheng <dannyzheng159@gmail.com>
2022-12-28 20:34:20 -0500

    complete whalesay shell script & add custom animals

---
+++ whalesay/run_whalesay.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+# Uncomment below to debug:
+# set -x 
+
+while true; do
+    read -p "Enter a message for whalesay (or 'exit' to quit or '-l' for list of animals): " message
+    if [ "$message" == "exit" ]; then
+        exit
+    elif [ "$message" == "-l" ]; then
+        /usr/games/cowsay -l
+    elif [ "$message" == "" ]; then
+        echo "You did not enter a message!"
+        continue
+    else
+        read -p "Enter an animal for whalesay (or default to whale) " animal
+        if [ "$animal" == "" ]; then
+            /usr/games/cowsay "$message"
+        else 
+        /usr/games/cowsay -f $animal "$message"
+        fi
+    fi
+done
+
+# To run the script within the same container after exiting:
+# start the container
+# run the command: docker exec -it <container-ID or name> /usr/local/bin/run_whalesay.sh
